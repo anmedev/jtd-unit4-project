135315b45ef179b681de2e9f8700ff5564c103a5
Anelle Mensah <aam2ds@virginia.edu>
2024-12-17 10:44:22 -0800

    fix detail page display bug

--- build-a-blog-site-with-spark/project-files/src/main/java/com/teamtreehouse/blog/dao/SimpleBlogDao.java
+++ build-a-blog-site-with-spark/project-files/src/main/java/com/teamtreehouse/blog/dao/SimpleBlogDao.java
@@ -5,11 +5,19 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SimpleBlogDao implements BlogDao{
+public class SimpleBlogDao implements BlogDao {
     private List<BlogEntry> entries;
 
     public SimpleBlogDao() {
-       entries = new ArrayList<>();
+        entries = new ArrayList<>();
+        seedEntries();
+    }
+
+    private void seedEntries() {
+        entries.add(new BlogEntry("The best day I’ve ever had", "The best day I've ever had content", LocalDate.parse("2024-10-01")));
+        entries.add(new BlogEntry("The absolute worst day I’ve ever had", "The absolute worst day I’ve ever had content", LocalDate.parse("2024-10-01")));
+        entries.add(new BlogEntry("That time at the mall", "That time at the mall content", LocalDate.parse("2024-10-01")));
+        entries.add(new BlogEntry("Dude, where’s my car?", "Dude, where’s my car? content", LocalDate.parse("2024-10-01")));
     }
 
     @Override
@@ -24,14 +32,8 @@ public class SimpleBlogDao implements BlogDao{
 
     @Override
     public List<BlogEntry> findAllEntries() {
-        List<BlogEntry> entries = new ArrayList<>();
-        entries.add(new BlogEntry("The best day I’ve ever had", "The best day I've ever had content", LocalDate.parse("2024-10-01")));
-        entries.add(new BlogEntry("The absolute worst day I’ve ever had", "The absolute worst day I’ve ever had content", LocalDate.parse("2024-10-01")));
-        entries.add(new BlogEntry("That time at the mall", "That time at the mall content", LocalDate.parse("2024-10-01")));
-        entries.add(new BlogEntry("Dude, where’s my car?", "Dude, where’s my car? content", LocalDate.parse("2024-10-01")));
-        return entries;
+        return new ArrayList<>(entries); // Return a copy of the entries list
     }
-
 
     @Override
     public BlogEntry findEntryBySlug(String slug) {
@@ -41,3 +43,41 @@ public class SimpleBlogDao implements BlogDao{
                 .orElseThrow(NotFoundException::new);
     }
 }
+
+
+//public class SimpleBlogDao implements BlogDao{
+//    private List<BlogEntry> entries;
+//
+//    public SimpleBlogDao() {
+//       entries = new ArrayList<>();
+//    }
+//
+//    @Override
+//    public boolean addEntry(BlogEntry blogEntry) {
+//        boolean isDuplicate = entries.stream()
+//                .anyMatch(blog -> blog.getSlug().equals(blogEntry.getSlug()));
+//        if (isDuplicate) {
+//            return false;
+//        }
+//        return entries.add(blogEntry);
+//    }
+//
+//    @Override
+//    public List<BlogEntry> findAllEntries() {
+//        List<BlogEntry> entries = new ArrayList<>();
+//        entries.add(new BlogEntry("The best day I’ve ever had", "The best day I've ever had content", LocalDate.parse("2024-10-01")));
+//        entries.add(new BlogEntry("The absolute worst day I’ve ever had", "The absolute worst day I’ve ever had content", LocalDate.parse("2024-10-01")));
+//        entries.add(new BlogEntry("That time at the mall", "That time at the mall content", LocalDate.parse("2024-10-01")));
+//        entries.add(new BlogEntry("Dude, where’s my car?", "Dude, where’s my car? content", LocalDate.parse("2024-10-01")));
+//        return entries;
+//    }
+//
+//
+//    @Override
+//    public BlogEntry findEntryBySlug(String slug) {
+//        return entries.stream()
+//                .filter(entry -> entry.getSlug().equals(slug))
+//                .findFirst()
+//                .orElseThrow(NotFoundException::new);
+//    }
+//}
